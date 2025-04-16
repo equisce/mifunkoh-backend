6f8031522872153fccc0a88d00737bb705b0fc9e
equisce <xiomara-ca@alumnos.cei.es>
2025-04-16 18:12:50 +0200

    Corrige nombre de variable MONGODB_URI

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 // Nos conectamos a MongoDB usando la URI del .env
 const conectarDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log('Conectado a MongoDB correctamente')
   } catch (error) {
     console.error('Error de conexion a MongoDB:', error)
